c1b14f30ec2dbdb48ad940be84f4a5bda5459ba1
vikasS45 <vsalunke758@gmail.com>
2021-10-25 17:48:21 +0530

    Hiding Navigation

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import React, {useState, useCallback} from 'react';
-import {Button, View, Alert} from 'react-native';
+import {Button, View, Alert, StyleSheet, Dimensions} from 'react-native';
 import YoutubePlayer from 'react-native-youtube-iframe';
+
+const width = Dimensions.get('screen').width;
 const App = () => {
   const [playing, setPlaying] = useState(false);
-  const togglePlaying = () => {
-    setPlaying((prev) => !prev);
-  }
+
   return (
     <View>
-      <YoutubePlayer
-        height={300}
-        play={playing}
-        videoId={'84WIaK3bl_s'}
-      />
+      <YoutubePlayer height={300} play={playing} videoId={'84WIaK3bl_s'} />
+      <View style={styles.hideDots} />
+      <View style={styles.hide} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  hide: {
+    backgroundColor: 'black',
+    height: 40,
+    width: 160,
+    marginTop: 120,
+    alignSelf: 'flex-end',
+    // marginRight:50,
+  },
+  hideDots: {
+    backgroundColor: 'transparent',
+    height: 40,
+    width: width,
+    marginTop: -300,
+    alignSelf: 'flex-end',
+  },
+});
 
 export default App;
